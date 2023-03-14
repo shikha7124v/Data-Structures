cab1789a10aabdae6b588d1524f86b55572ae0eb
shikha7124 <shikhaverma7124@gmail.com>
2023-03-14 20:22:16 +0530

    Array/setZeroes

--- Array/setZeroes.cpp
+++ Array/setZeroes.cpp
@@ -2,6 +2,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 // Approach 1
+// Time Complexity: O(N*M) + O(N+M)
+// Space Complexity: O(1)
 void setZeroes(vector<vector<int>>& matrix){
     int rows = matrix.size(), cols = matrix.size();
     for(int i=0; i<rows; i++){
@@ -77,4 +79,31 @@ int main()
 //                 }
 //             }
 //         }
+//     }
+// Time Complexity: O(N*M + N*M)
+// Space Complexity: O(N)
+// ---------------------------------------------------------------------------
+// Approach3
+// void setZeroes(vector<vector<int>>& matrix) {
+//         int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+//         for (int i = 0; i < rows; i++) {
+//             if (matrix[i][0] == 0) col0 = 0;
+//             for (int j = 1; j < cols; j++) {
+//                 if (matrix[i][j] == 0) {
+//                     matrix[i][0] = 0;
+//                     matrix[0][j] = 0;
+//                 }
+//             }
+//         }
+//         for (int i = rows - 1; i >= 0; i--) {
+//             for (int j = cols - 1; j >= 1; j--) {
+//                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//             if (col0 == 0) {
+//                 matrix[i][0] = 0;
+//             }
+//         }
+
 //     }
