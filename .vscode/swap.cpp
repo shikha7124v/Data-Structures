c657a5fa58d403c56a35cb79e8a844d022928dda
shikha7124 <shikhaverma7124@gmail.com>
2023-03-31 10:25:36 +0530

    Swapped program

---
+++ .vscode/swap.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a =10, b=20;
+    //logic1 using temp variable
+    int temp = a;
+    a = b;
+    b = temp;
+    cout<<"Swapped with logic 1 = "<<a<<b;
+    //logic2 without using temp variable with + and -
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    cout<<"Swapped with logic 2 = "<<a<<b;
+    //logic3 with * and /
+    a = a*b;
+    b = a/b;
+    a = a/b;
+    cout<<"Swapped with logic 3 = "<<a<<b;
+    //logic4 with Bitwise XOR ^
+    a = a^b;
+    b = a^b;
+    a = a^b;
+    cout<<"Swapped with logic 4 = "<<a<<b;
+    //logic5 within a sinfle line
+    b = a+b - (a=b);
+    cout<<"Swapped with logic 5 = "<<a<<b;
+    return 0;
+}
