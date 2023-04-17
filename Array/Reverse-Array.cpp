95b63c076f4133f0e26a1f4ca9ea21ae02204d89
shikha7124 <shikhaverma7124@gmail.com>
2023-04-17 15:19:05 +0530

    Below code by recursion

--- Array/Reverse-Array.cpp
+++ Array/Reverse-Array.cpp
@@ -1,5 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+int reverseTheArray(int start, int end, int n, int arr[])
+{
+    //iterative method 
+    
+    // while(start < end)
+    // {
+    //     int temp = arr[start];
+    //     arr[start] = arr[end];
+    //     arr[end] = temp;
+
+    //     start++;
+    //     end--;
+    // }
+
+    //recursive way
+    if(start >= end){
+        return 0;
+    }
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp; 
+
+    reverseTheArray(start+1, end-1, n, arr);
+}
 int main()
 {
     int arr[] = {2,3,4,5};
@@ -9,21 +33,14 @@ int main()
         cout<<arr[i]<<" ";
     }
     cout<<endl;
-    //iterative method 
     int start = 0; 
     int end = n-1;
-    while(start < end)
-    {
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-
-        start++;
-        end--;
-    }
+    
+    reverseTheArray(start, end, n, arr);
     printf("Reversed Array : \n");
     for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
+    return 0;
 }
