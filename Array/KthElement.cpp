b78c129ae11be2bbf2eb1b8b4d32fab30f4446c8
shikha7124 <shikhaverma7124@gmail.com>
2023-04-17 21:49:40 +0530

    Kth smallest element using set data structure

--- Array/KthElement.cpp
+++ Array/KthElement.cpp
@@ -1,18 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    int arr[] ={12,3,4,6};
-    int n = sizeof(arr)/sizeof(int);
+    int arr[] = {12, 3, 4, 6};
+    int n = sizeof(arr) / sizeof(int);
     int k = 2;
 
-    set<int> s(arr, arr+n);
-    //s.begin() returns a pointer to first
-    // elements in the set 
+    set<int> s(arr, arr + n);
+    // s.begin() returns a pointer to first
+    //  elements in the set
 
-set<int>::iterator itr = s.begin();
-advance(itr, k-1);  //itr points to kth element in set
-cout<<*itr<<"\n";
-return 0;
+    set<int>::iterator itr = s.begin();
+    advance(itr, k - 1); // itr points to kth element in set
+    cout << *itr << "\n";
+    return 0;
 }
+
