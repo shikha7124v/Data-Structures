69ff7d67bf6197f21fdae258915ec0664fc7553e
shikha7124 <shikhaverma7124@gmail.com>
2023-06-28 22:29:21 +0530

    Selection Sort

--- Sorting Techniques/Sorting 1/selectionSort.cpp
+++ Sorting Techniques/Sorting 1/selectionSort.cpp
@@ -1,6 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-int selectionSort(int arr, int n)
+int selectionSort(int arr[], int n)
 {
-    
+    for(int i=0; i<n-1; i++){
+        int mini = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j] <arr[mini]){
+                mini = j;
+            }
+        }
+        swap(arr[mini], arr[i]);
+    }
+    return 0;
+}
+int main()
+{
+    int arr[] = {19,23,11,18,17,10};
+    int n = sizeof(arr)/sizeof(int);
+    for(int i=0;i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    selectionSort(arr, n);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
 }
