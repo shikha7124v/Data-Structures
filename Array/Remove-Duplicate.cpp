3ee2a111a4a38e0e33227a2fb74b5f696b76f4a6
shikha7124 <shikhaverma7124@gmail.com>
2023-07-04 17:18:11 +0530

    Remove Duplicate from the array using set and 2 pointers

--- Array/Remove-Duplicate.cpp
+++ Array/Remove-Duplicate.cpp
@@ -3,18 +3,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int removeDuplicates(vector<int>& nums){
+// int removeDuplicates(vector<int>& nums){
+//     if(nums.size() == 0){
+//         return 0;
+//     }
+//     int i=0; 
+//     for(int j=1; j<nums.size(); j++){
+//         if(nums[j] != nums[i]){
+//             i++;
+//             nums[i] = nums[j];
+//         }
+//     }
+//     return i+1;  //length of the sorted array after removing the duplicates
+// }
+int removeDuplicates(vector<int> &nums){
     if(nums.size() == 0){
         return 0;
     }
-    int i=0; 
-    for(int j=1; j<nums.size(); j++){
-        if(nums[j] != nums[i]){
-            i++;
-            nums[i] = nums[j];
-        }
+    set<int> s;
+    int j = 0;
+    for(int i=0; i<nums.size(); i++){
+        s.insert(nums[i]);
     }
-    return i+1;  //length of the sorted array after removing the duplicates
+    int k = s.size();
+    for(int x: s){
+        nums[j++] = x;
+    }
+    return k;
 }
 int main()
 {
@@ -23,4 +38,7 @@ int main()
     for(int i=0; i<k; i++){
         cout<<nums[i]<<" ";
     }
+    cout<<endl;
+    cout<<"Elements in the array after removing all the duplicates : "<<k;
+    return 0;
 }
