16b07d9f9a0e98a99745b8bb40a7e3c47350deb1
shikha7124 <shikhaverma7124@gmail.com>
2023-07-14 07:33:37 +0530

    appearance one

---
+++ Array/Basic/number_appearce_one_other_twice/brute-force-approach.cpp
@@ -0,0 +1,28 @@
+// Find the number which appearce once in the array
+// Naive Approach (Brute Force Approach) - Will do the linear search and count the occurences of the elements if the occurrence will be one will return that element.
+#include<bits/stdc++.h>
+using namespace std;
+
+int appearceOnce(vector<int> arr, int n){
+    for(int i=0; i<n; i++){
+        int num = arr[i];
+        int cnt = 0;
+        for(int j=0; j<n; j++){
+            if(arr[j] == num){
+                cnt++;
+            }
+        }
+        if(cnt == 1){
+            return num;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> arr{1,1,2,2,3,3,4,4,5};
+    int n = arr.size();
+
+    cout<<appearceOnce(arr, n);
+    return 0;
+}
