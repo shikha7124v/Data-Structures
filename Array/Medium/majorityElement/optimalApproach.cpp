87732c88b01a172ea214a11d9e32396c6f191e94
shikha7124 <shikhaverma7124@gmail.com>
2023-07-19 22:00:27 +0530

    majority element

---
+++ Array/Medium/majorityElement/optimalApproach.cpp
@@ -0,0 +1,13 @@
+//  Moore’s Voting Algorithm:
+//  Approach: 
+// 1. Initialize 2 variables:
+//    Count –  for tracking the count of element
+//    Element – for which element we are counting
+// 2. Traverse through the given array.
+//    -> If Count is 0 then store the current element of the array as Element.
+//    -> If the current element and Element are the same increase the Count by 1.
+//    -> If they are different decrease the Count by 1.
+// 3. The integer present in Element should be the result we are expecting 
+#include<bits/stdc++.h>
+using namespace std;
+
