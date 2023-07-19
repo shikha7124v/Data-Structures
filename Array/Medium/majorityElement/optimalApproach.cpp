6f2f233dc7e09380830c167f4f2406446594c51b
shikha7124 <shikhaverma7124@gmail.com>
2023-07-19 22:07:28 +0530

    majority element

--- Array/Medium/majorityElement/optimalApproach.cpp
+++ Array/Medium/majorityElement/optimalApproach.cpp
@@ -11,3 +11,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int majorityElement(vector<int> v){
+    int n = v.size();
+    int cnt = 0;
+    int el = 0;
+    for(int i=0; i<n; i++){
+        if(cnt == 0){
+            cnt = 1;
+            el = v[i];
+        }
+        else if(el == v[i]){
+            cnt++;
+        }
+        else{
+            cnt--;
+        }
+    }
+    int cnt1 = 0;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == el) cnt1++;
+    }
+
+    if (cnt1 > (n / 2)) return el;
+    return -1;
+}
+int main(){
+    vector<int> arr{1,2,4,3,2,1,1,1,1};
+    int n = arr.size();
+    cout<<majorityElement(arr);
+    return 0;
+}
