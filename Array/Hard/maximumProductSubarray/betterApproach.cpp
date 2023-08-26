1f98564dbb6d32f0802b526be373bdb5cee6e9dc
shikha7124 <shikhaverma7124@gmail.com>
2023-08-26 09:36:16 +0530

    maxProdSubarray

---
+++ Array/Hard/maximumProductSubarray/betterApproach.cpp
@@ -0,0 +1,24 @@
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProductSubArray(vector<int>& nums) {
+    int result = nums[0];
+    for(int i=0;i<nums.size()-1;i++) {
+        int p = nums[i];
+        for(int j=i+1;j<nums.size();j++) {
+           result = max(result,p);
+           p *= nums[j];
+        }
+        result = max(result,p);//manages (n-1)th term 
+    }
+    return result;
+}
+
+int main() {
+    vector<int> nums = {1,2,-3,0,-4,-5};
+    cout<<"The maximum product subarray: "<<maxProductSubArray(nums);
+    return 0;
+}
