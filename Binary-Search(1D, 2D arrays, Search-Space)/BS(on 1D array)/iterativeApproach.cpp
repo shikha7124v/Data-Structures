02006a832fc6ac2e43216a24f0a3fa805cd6de10
shikha7124 <shikhaverma7124@gmail.com>
2023-08-28 22:27:01 +0530

    binarySearch

---
+++ Binary-Search(1D, 2D arrays, Search-Space)/BS(on 1D array)/iterativeApproach.cpp
@@ -0,0 +1,21 @@
+// Implementation of Binary Search algorithm for using iterative approach
+#include<bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int> &nums, int target, int low, int high){
+    while(low <= high){
+        int mid = (low+high)/2;
+        if(nums[mid] == target)  return mid;
+        else if(target > nums[mid]) low = mid+1;
+        else if(target < nums[mid]) high = mid-1;
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> nums{1,4, 5, 6, 8, 9};
+    int n = nums.size();
+    int target = 4;
+    cout<<"The index where the element found using BinarySearch is "<<binarySearch(nums, target, 0, n-1);
+    return 0;
+}
