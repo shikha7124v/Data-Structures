6372396c880ba9d21084e6379291894ffd17e1db
shikha7124 <shikhaverma7124@gmail.com>
2023-09-14 17:00:44 +0530

    added

---
+++ Binary-Search(1D, 2D arrays, Search-Space)/BS-On-Answers/optimal1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int squareRoot(int n){
+    int ans = 0;
+    ans = sqrt(n);
+    return ans;
+}
+int main()
+{
+    int n;
+    cin>>n; 
+    cout<<squareRoot(n);
+    return 0;
+}
