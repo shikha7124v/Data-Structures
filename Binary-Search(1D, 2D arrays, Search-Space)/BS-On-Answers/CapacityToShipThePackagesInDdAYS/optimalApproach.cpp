244c93cc0aada2191ca91da52fccae039b54bca0
shikha7124 <shikhaverma7124@gmail.com>
2023-10-10 20:46:30 +0530

    added

---
+++ Binary-Search(1D, 2D arrays, Search-Space)/BS-On-Answers/CapacityToShipThePackagesInDdAYS/optimalApproach.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findDays(vector<int> &weights, int cap){
+    int days = 1;
+    int load = 0;
+    int n = weights.size();
+    for(int i=0; i<n; i++){
+        if(load + weights[i] > cap){
+            days += 1;
+            load = weights[i];
+        }
+        else{
+            load += weights[i];
+        }
+    }
+    return days;
+}
+int leastWeightCapacity(vector<int> &weights, int d){
+    int low = *max_element(weights.begin(), weights.end());
+    int high = accumulate(weights.begin(), weights.end(), 0);
+    while(low <= high){
+        int mid = (low+high)/2;
+        int numberOfDays = findDays(weights, mid);
+        if(numberOfDays <= d){
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+    }
+    return low;
+}
+int main()
+{
+    vector<int> arr{5,4,5,2,3,4,5,6};
+    int d = 5; //days
+    int ans = leastWeightCapacity(arr, d);
+    cout<<"The minimum capacity should be: "<<ans<<endl;
+    return 0;
+}
