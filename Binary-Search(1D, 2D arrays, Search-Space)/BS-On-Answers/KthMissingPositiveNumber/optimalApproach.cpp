976504312a1a8213129a4292df839979248c7fa3
shikha7124 <shikhaverma7124@gmail.com>
2023-10-10 23:29:19 +0530

    added

---
+++ Binary-Search(1D, 2D arrays, Search-Space)/BS-On-Answers/KthMissingPositiveNumber/optimalApproach.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int missing(vector<int>vec, int n, int k){
+    int low = 0, high = n-1;
+    while(low <= high){
+        int mid = (low + high)/2;
+        int missing = vec[mid] - (mid + 1);
+        if(missing < k){
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    return k+high+1;
+}
+int main()
+{
+    vector<int> vec = {4,7,9,10};
+    int n = 4, k = 4;
+    int ans = missing(vec, n, k);
+    cout<<"The missing number is: "<<ans<<endl;
+    return 0;
+}
